34dddb43253cb5c4ef3d85e412a205d077d00317
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-22 14:27:23 +0530

    Create rg_creation.ps1

---
+++ notebooks/Shared/test_folder/rg_creation.ps1
@@ -0,0 +1,7 @@
+Param(
+    [string] $ResourceGroupName = 'Suvamrgnewrg',
+    [string] $TemplateFile = 'testnew.json',
+    [string] $TemplateParametersFile = 'testnew.parameters.json',
+)
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParametersFile
