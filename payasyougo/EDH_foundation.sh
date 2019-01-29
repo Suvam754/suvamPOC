96ccd3eda11a40c25e4a3761d5d2cb814aeb015b
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-29 21:00:55 +0530

    Update EDH_foundation.sh

--- payasyougo/EDH_foundation.sh
+++ payasyougo/EDH_foundation.sh
@@ -2,4 +2,5 @@
 echo "Please find the updated files"
 echo "Please find the updated files"
 sed -i 's/development/'$1'/g' rg_creation.ps1
+echo "Please find the updated files"
 sed -i 's/development/'$1'/g' fndcmpnt.parameterupdated.json
